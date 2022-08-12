8337b94e33eeca533f8aab78903480209e3dabe1
pathumlakshan1101 <lakshanherath1101@gmail.com>
2022-08-12 21:07:13 +0530

    sever fxml and controller

---
+++ src/com/ijse/chat/controller/SeverController.java
@@ -0,0 +1,25 @@
+package com.ijse.chat.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class SeverController {
+    public AnchorPane ChatRoomContext;
+    public JFXTextField txtTypeMessage;
+    public Button btnSend;
+    public ImageView cameraIcon;
+    public Label lblName;
+    public VBox vbox;
+
+    public void SendOnAction(ActionEvent actionEvent) {
+    }
+
+    public void Camera_On_Mouse_Clicked(MouseEvent mouseEvent) {
+    }
+}
